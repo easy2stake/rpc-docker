98441d771f821f32c76cbeecf6592e8a9f54e2df
Alex <alex@easy2stake.com>
2025-09-26 16:57:33 +0300

    Adding sonic-heal tools.

--- ftm/ftm-heal.sh
+++ ftm/ftm-heal.sh
@@ -2,7 +2,7 @@
 
 # Stop the main ftm service if it's running
 echo "Stopping ftm service..."
-docker compose -f docker-compose.yml down ftm
+docker compose -f docker-compose.yml down
 
 # Run the ftm-heal service
 echo "Running ftm-heal command in detached mode..."
