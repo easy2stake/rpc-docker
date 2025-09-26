846ab8b7a9a323d6e949a2ad3f5bc3740c1a1fd5
Alex <alex@easy2stake.com>
2025-09-26 16:49:50 +0300

    Add a ftm heal container.

---
+++ ftm/ftm-heal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Stop the main ftm service if it's running
+echo "Stopping ftm service..."
+docker-compose -f docker-compose.yml down ftm
+
+# Run the ftm-heal service
+echo "Running ftm-heal command in detached mode..."
+docker-compose -f docker-compose-heal.yml run --rm -d ftm-heal
+
+echo "ftm-heal command started in background."
