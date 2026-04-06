a33a98f9d774187433a878dad0ba24a0a4c307b1
Alex <alex@easy2stake.com>
2026-04-06 18:53:59 +0300

    Update override-gossip.sh

--- hyperliquid/override-gossip.sh
+++ hyperliquid/override-gossip.sh
@@ -1,4 +1,4 @@
-STATIC_PEERS='["173.231.43.98","136.243.70.112"]'
+STATIC_PEERS='["173.231.43.98","136.243.70.112","142.132.131.162","15.235.226.235"]'
 
 curl -s -X POST --header "Content-Type: application/json" --data '{ "type": "gossipRootIps" }' https://api.hyperliquid.xyz/info \
   | jq -c --argjson static "$STATIC_PEERS" \
